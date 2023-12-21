78a9f7adbb0b2b5eae8c7db0ced5f8860925c165
Felipe Lago <132310712+eufelago@users.noreply.github.com>
2023-12-20 23:55:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,112 @@
-def soma(x, y):
-  return x + y
+# importando tkinter
+from tkinter import * # asterisco significa importar tudo que existe dentro daquela biblioteca no caso a "tkinter"
+from tkinter import ttk
 
-def subtracao(x, y):
-  return x - y
+#cores
+cor1 = "#3b3b3b" #preta
+cor2 = "#feffff" #branca
+cor3 = "#38576b" #azul carregado
+cor4 = "#ECEFF1" #cizenta 
+cor5 = "#FFAB40" #laranja
 
-def multiplicacao(x, y):
-  return x * y
+#criando janela
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("235x310")
+janela.config(bg = cor1)
 
-def divisao(x, y):
-  if y != 0:
-      return x / y
-  else:
-      return "Erro: Divisão por zero!"
+#dividindo frames
+frame_tela = Frame(janela, width=235, height=50, bg=cor3)
+frame_tela.grid(row=0, column=0)
 
-def calculadora():
-  print("Selecione a operação:")
-  print("1. soma")
-  print("2. Subtração")
-  print("3. Multiplicação")
-  print("4. Divisão")
+#corpo para botões
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
 
-  escolha = input("Digite 1, 2, 3 ou 4: ")
 
-  if escolha not in ('1', '2', '3', '4'):
-      print("Escolha inválida. Tente novamente.")
-      return
 
-  num1 = float(input("Digite o primeiro número: "))
-  num2 = float(input("Digite o segundo número: "))
 
-  if escolha == '1':
-      print(num1, "+", num2, "=", soma(num1, num2))
-  elif escolha == '2':
-      print(num1, "-", num2, "=", subtracao(num1, num2))
-  elif escolha == '3':
-      print(num1, "*", num2, "=", multiplicacao(num1, num2))
-  elif escolha == '4':
-      resultado = divisao(num1, num2)
-      if isinstance(resultado, float):
-          print(num1, "/", num2, "=", resultado)
-      else:
-          print(resultado)
+todos_valores = ''
+valor_texto =StringVar()
 
-# Chamar a função principal
-calculadora()
+#criando função 
+def entrada_valores(event):
+
+    global todos_valores
+    todos_valores = todos_valores + str(event)
+ 
+    #PASSANDO O VALOR PARA A TELA
+    valor_texto.set(todos_valores)
+
+
+
+#função para calcular
+    
+
+
+def calcular():
+
+    global todos_valores
+    resultado = eval(todos_valores)
+    valor_texto.set(str(resultado))
+    
+
+def limpar_tela():
+    
+    global todos_valores
+    todos_valores = ""
+    valor_texto.set("")
+
+#CRIANDO LABEL
+app_label = Label(frame_tela, textvariable = valor_texto, width=16, height=2, padx=7, relief=FLAT, anchor="e", justify=RIGHT, font='Ivy 18', bg=cor3, fg=cor2)
+app_label.place(x=0,y=0)
+
+
+#criando botões
+b_1 = Button(frame_corpo, command=limpar_tela, text="C", width=11, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_1.place(x=0, y=0)
+b_2 = Button(frame_corpo, command= lambda: entrada_valores('%'), text="%", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_2.place(x=118, y=0)
+b_3 = Button(frame_corpo, command= lambda: entrada_valores('/'), text="/", width=5, height=2, bg=cor5, fg=cor2 , font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_3.place(x=177, y=0)
+
+
+b_4 = Button(frame_corpo, command= lambda: entrada_valores('7'), text="7", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_4.place(x=0, y=52)
+b_5 = Button(frame_corpo, command= lambda: entrada_valores('8'), text="8", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_5.place(x=59, y=52)
+b_6 = Button(frame_corpo, command= lambda: entrada_valores('9'), text="9", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_6.place(x=118, y=52)
+
+b_7 = Button(frame_corpo, command= lambda: entrada_valores('*'), text="X", width=5, height=2, bg=cor5, fg=cor2 , font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_7.place(x=177, y=52)
+
+b_8 = Button(frame_corpo, command= lambda: entrada_valores('4'), text="4", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_8.place(x=0, y=104)
+b_9 = Button(frame_corpo, command= lambda: entrada_valores('5'), text="5", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_9.place(x=59, y=104)
+b_10 = Button(frame_corpo, command= lambda: entrada_valores('6'), text="6", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_10.place(x=118, y=104)
+
+b_11 = Button(frame_corpo, command= lambda: entrada_valores('-'), text="-", width=5, height=2, bg=cor5, fg=cor2 , font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_11.place(x=177, y=104)
+
+b_12 = Button(frame_corpo, command= lambda: entrada_valores('1'), text="1", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_12.place(x=0, y=156)
+b_13= Button(frame_corpo, command= lambda: entrada_valores('2'), text="2", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_13.place(x=59, y=156)
+b_14 = Button(frame_corpo, command= lambda: entrada_valores('3'), text="3", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_14.place(x=118, y=156)
+
+b_15 = Button(frame_corpo, command= lambda: entrada_valores('+'), text="+", width=5, height=2, bg=cor5, fg=cor2 , font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_15.place(x=177, y=156)
+
+
+b_3 = Button(frame_corpo, command= lambda: entrada_valores('0'), text="0", width=11, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_3.place(x=0, y=208)
+b_14 = Button(frame_corpo, command= lambda: entrada_valores('.'), text=".", width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_14.place(x=118, y=208)
+b_14 = Button(frame_corpo, command= calcular, text="=", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_14.place(x=177, y=208)
+
+janela.mainloop() 
